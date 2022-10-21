29d7f734b79d98430c327350af41054a75ef3d9b
Jordan <jordanhoward1991@gmail.com>
2022-10-21 15:40:10 -0600

    addes links for navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { NavLink, Link } from "react-router-dom";
 
-import '../src/styles/App.scss';
-import '../src/components/pages/home'
-
+import "../src/styles/App.scss";
+import "../src/components/pages/home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div className="navbar">
-          <h3>Home</h3>
-          <h3>Contact</h3>
-          <h3>Menu</h3> 
+          <nav>
+            <a href="/home/">Home</a>
+            <a href="/about-me/">About Me</a>
+            <a href="/contact/">Contact</a>
+          </nav>
         </div>
         <hr></hr>
       </header>
